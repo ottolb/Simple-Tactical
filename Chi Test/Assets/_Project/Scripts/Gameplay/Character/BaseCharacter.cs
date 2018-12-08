e23b3d9bf1035126f31fd2ea8b0cd3a578ac4fad
Otto Braitback <ottolb@gmail.com>
2018-12-08 18:12:40 -0200

    • Character initial architectur

---
+++ Chi Test/Assets/_Project/Scripts/Gameplay/Character/BaseCharacter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class BaseCharacter : MonoBehaviour, ICharacter
+    {
+        public float life;
+        public float speed;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Die()
+        {
+
+        }
+
+        public void Init()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+
+        public void TakeDamage(float p_amount)
+        {
+
+        }
+    }
+}
---
+++ Chi Test/Assets/_Project/Scripts/Gameplay/Character/ICharacter.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Gameplay
+{
+    public interface ICharacter
+    {
+
+        void Init();
+
+        void Move();
+
+        void Die();
+
+        void TakeDamage(float p_amount);
+
+    }
+}
