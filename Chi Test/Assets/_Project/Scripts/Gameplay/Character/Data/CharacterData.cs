062019071ff4700fa6e8acb6299a48421dd1a993
Otto Braitback <ottolb@gmail.com>
2018-12-11 17:14:38 -0200

    • Character Data

---
+++ Chi Test/Assets/_Project/Scripts/Gameplay/Character/Data/CharacterData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class CharacterData : ScriptableObject
+    {
+        public int totalActions;
+        private int _availableActios;
+
+        public int life;
+        protected int currentLife;
+        public float speed;
+        public float moveArea;
+
+        public int attackForce;
+        public float attackRange;
+
+    }
+}
